8db52797dc9421c0cfcdc6f2ca06e68229e76b15
JorgeE <je.suarezg1@uniandes.edu.co>
2022-10-17 12:20:38 -0500

    Corrección de ortografía

--- src/servicios/servicios.py
+++ src/servicios/servicios.py
@@ -79,8 +79,8 @@ class Auth(Resource):
             return {"resultado": "ERROR", "mensaje": "La clave suministrada no cumple criterios mínimos. Por favor suministre una clave \n1%"+
         "con las siguientes características: \n1%"+
         "8 o más caracteres \n1%"+
-        "1 o más digitos \n1%"+
-        "1 o más simbolos \n1%"+
+        "1 o más dígitos \n1%"+
+        "1 o más símbolos \n1%"+
         "1 o más letras mayúsculas \n1%"+
         "1 o más letras minúsculas"}, 400
 
