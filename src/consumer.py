8a153885627a3fa98407ad47d51abdb81168bdfc
Jorge Enrique Suárez González <je.suarezg1@uniandes.edu.co>
2022-11-20 14:59:30 -0500

    Cambios por consumo de mensajes pub/sub

--- src/consumer.py
+++ src/consumer.py
@@ -2,44 +2,34 @@ import pika, sys, os, json
 import requests
 
 def main():
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS']= '../../audioconverter-service-key.json'
     """Receives messages from a pull subscription."""
-    # [START pubsub_subscriber_async_pull]
-    # [START pubsub_quickstart_subscriber]
     from concurrent.futures import TimeoutError
     from google.cloud import pubsub_v1
 
     project_id = "audioconverter-366014"
     subscription_id = "SuscriptorWorker"
-    # Number of seconds the subscriber should listen for messages
-    timeout = 5.0
 
     subscriber = pubsub_v1.SubscriberClient()
-    # The `subscription_path` method creates a fully qualified identifier
-    # in the form `projects/{project_id}/subscriptions/{subscription_id}`
     subscription_path = subscriber.subscription_path(project_id, subscription_id)
 
     def callback(message: pubsub_v1.subscriber.message.Message) -> None:
         print(f"Received {message}.")
-        bodyAsJson = json.loads(body.decode())
+        print(message.data.decode("utf-8").replace("'","\""))
+        bodyAsJson = json.loads(message.data.decode("utf-8").replace("'","\""))
         x = requests.post (url = "http://127.0.0.1:4000/api/convert",json = bodyAsJson)
-        #print(x)
-        print("Done")
         message.ack()
+        print("Done")
 
     streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
     print(f"Listening for messages on {subscription_path}..\n")
 
-    # Wrap subscriber in a 'with' block to automatically call close() when done.
     with subscriber:
         try:
-            # When `timeout` is not set, result() will block indefinitely,
-            # unless an exception is encountered first.
-            streaming_pull_future.result(timeout=timeout)
+            streaming_pull_future.result()
         except TimeoutError:
             streaming_pull_future.cancel()  # Trigger the shutdown.
             streaming_pull_future.result()  # Block until the shutdown is complete.
-    # [END pubsub_subscriber_async_pull]
-    # [END pubsub_quickstart_subscriber]
 
 if __name__ == '__main__':
     try:
