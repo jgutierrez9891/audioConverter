c1bd4e591cea3ea83ee3c2c4dd8a611e0a177878
Jorge Enrique Suárez González <je.suarezg1@uniandes.edu.co>
2022-11-27 13:36:03 -0500

    Sin consumer

--- srcConverter/app.py
+++ srcConverter/app.py
@@ -28,42 +28,8 @@ def getconn():
         )
         return conn
 
-def consumer():
-    """Receives messages from a pull subscription."""
-    from concurrent.futures import TimeoutError
-    from google.cloud import pubsub_v1
-
-    project_id = "audioconverter-366014"
-    subscription_id = "SuscriptorWorker"
-
-    subscriber = pubsub_v1.SubscriberClient()
-    subscription_path = subscriber.subscription_path(project_id, subscription_id)
-
-    def callback(message: pubsub_v1.subscriber.message.Message) -> None:
-        print(f"Received {message}.")
-        print(message.data.decode("utf-8").replace("'","\""))
-        bodyAsJson = json.loads(message.data.decode("utf-8").replace("'","\""))
-        x = requests.post (url = "http://127.0.0.1:5000/api/convert",json = bodyAsJson)
-        message.ack()
-        print("Done")
-
-    streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
-    print(f"Listening for messages on {subscription_path}..\n")
-
-    with subscriber:
-        try:
-            streaming_pull_future.result()
-        except TimeoutError:
-            streaming_pull_future.cancel()  # Trigger the shutdown.
-            streaming_pull_future.result()  # Block until the shutdown is complete.
-
-
 app = Flask(__name__)
-t = Thread(target=consumer)
-t.daemon = True
-t.start()
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS']= '../../audioconverter-service-key.json'
 app.config['EMAIL_API_KEY'] = os.environ['EMAIL_API_KEY']
 app.config['GCP_BUCKET_NAME'] = "audioconverter-files"
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+pg8000://"
